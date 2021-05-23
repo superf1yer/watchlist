a0c265e521701dacbd88d40d343eb7d20ce2b2f8
superf1yer <524512470@qq.com>
2021-05-24 06:41:09 +0800

    add index page

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-from flask import Flask, escape, url_for
+#_*_coding=utf-8_*_
+from flask import Flask, render_template
 app = Flask(__name__)
-
+   
+name = 'Jack Lin'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 @app.route('/')
 def index():
-    return "Welcome to my Watchlist"
-    
-@app.route('/user/<name>')
-def user_name(name):
-    return "username: %s"% escape(name)
-
-@app.route('/<name>/<int:age>')
-def age(name, age):
-    return "username: %s, age : %d" % (name, age+20)
-    
-@app.route('/test')
-def test_url_for():
-    print(url_for('index'))
-    print(url_for('user_name', name='greyli'))    
-    print(url_for('index', num=1))
-    print(url_for('test_url_for'))
-    return 'Test page'
-
+    return render_template('index.html', name=name, movies=movies)
