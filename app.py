9f55c31f3c3da8a95d257799923446364c9a0c6e
superf1yer <524512470@qq.com>
2021-05-29 06:01:01 +0800

    add base template and error template

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
 #_*_coding=utf-8_*_
+import os 
+import sys
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
 app = Flask(__name__)
-   
-name = 'Jack Lin'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path ,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return {'user' : user}
+
+@app.errorhandler(404)
+def page_not_found(e):
+    user = User.query.first()
+    return render_template('404.html'), 404
+    
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+    
